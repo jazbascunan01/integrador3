8f90e409376a1f1dd918852c3976dee9e9decee2
Mateo Fernandez <emfernandez@alumnos.exa.unicen.edu.ar>
2025-05-10 11:10:32 -0300

    Se utiliza EstudianteResponseDTO para responses de listas de Estudiantes

--- src/main/java/com/example/integrador3/service/EstudianteService.java
+++ src/main/java/com/example/integrador3/service/EstudianteService.java
@@ -2,9 +2,11 @@ package com.example.integrador3.service;
 
 import com.example.integrador3.model.Estudiante;
 import com.example.integrador3.repository.EstudianteRepository;
+import com.example.integrador3.service.dto.estudiante.EstudianteResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,12 +27,25 @@ public class EstudianteService {
     }
 
     // TODO Usar DTO sin carreras
-    public List<Estudiante> findAll() {
-        return estudianteRepository.findAll();
+    public List<EstudianteResponseDTO> findAll() {
+        List<Estudiante> estudiantes = estudianteRepository.findAll();
+        List<EstudianteResponseDTO> estudiantesResponse = new ArrayList<>();
+        for (Estudiante estudiante : estudiantes) {
+            EstudianteResponseDTO estudianteResponse = new EstudianteResponseDTO();
+            estudianteResponse.setDNI(estudiante.getDNI());
+            estudianteResponse.setNombre(estudiante.getNombre());
+            estudianteResponse.setApellido(estudiante.getApellido());
+            estudianteResponse.setEdad(estudiante.getEdad());
+            estudianteResponse.setGenero(estudiante.getGenero());
+            estudianteResponse.setCiudad(estudiante.getCiudad());
+            estudianteResponse.setLU(estudiante.getLU());
+            estudiantesResponse.add(estudianteResponse);
+        }
+        return estudiantesResponse;
     }
 
-    public List<Estudiante> findAllByDni() {
-        List<Estudiante> estudiantes = estudianteRepository.findAll();
+    public List<EstudianteResponseDTO> findAllByDni() {
+        List<EstudianteResponseDTO> estudiantes = this.findAll();
         estudiantes.sort((e1, e2) -> e1.getDNI().compareTo(e2.getDNI()));
         return estudiantes;
     }
--- src/main/java/com/example/integrador3/service/dto/estudiante/EstudianteResponseDTO.java
+++ src/main/java/com/example/integrador3/service/dto/estudiante/EstudianteResponseDTO.java
@@ -1,4 +1,18 @@
 package com.example.integrador3.service.dto.estudiante;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class EstudianteResponseDTO {
+    private Integer DNI;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String genero;
+    private String ciudad;
+    private int LU;
 }
